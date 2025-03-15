8fe98c1369ab251c0a4110d7ecf91f8bf042be12
Hemanthmeesala <hemanth.meesala2004@gmail.com>
2025-03-15 11:40:35 +0530

    Completed switch Statements

---
+++ Angular-Tutorial-07/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Angular-Tutorial-07';
+  colour = 'green'
+}
